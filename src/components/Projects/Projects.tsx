8864cb063415ed3fa781da9165e5fb1651ae90f6
julio-silveira <julioqueiroz15@gmail.com>
2022-11-09 14:10:11 -0300

    feat: Lógica inicial do componente de Projetos configuradas

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,5 +1,23 @@
 import * as React from 'react'
+import projectList from '../../data/projectList'
 
 export default function Projects() {
-  return <div>Projects</div>
+  return (
+    <section>
+      <h2>Projects</h2>
+      {projectList.map(({ name, description, image, urlDeploy, urlGithub }) => (
+        <article key={name}>
+          <img width={100} src={image} alt={`image-${name}`} />
+          <h3>{name}</h3>
+          <p>{description}</p>
+          <a href={urlGithub}>
+            <button type="button">Go to Repository</button>
+          </a>
+          <a href={urlDeploy}>
+            <button type="button">Visualizar site </button>
+          </a>
+        </article>
+      ))}
+    </section>
+  )
 }

ea11a1e357a6e6696f18a0b6065f20377ae4381e
julio-silveira <julioqueiroz15@gmail.com>
2022-11-09 16:13:09 -0300

    feat: Titulos adicionados no componente de techs

--- src/components/Techs/Techs.tsx
+++ src/components/Techs/Techs.tsx
@@ -1,3 +1,4 @@
+import { Typography } from '@mui/material'
 import * as React from 'react'
 import { techBarList, techNamesList } from '../../data/techslist'
 import ProgressBar from './ProgeressBar'
@@ -5,11 +6,13 @@ import ProgressBar from './ProgeressBar'
 export default function Techs() {
   return (
     <section>
+      <Typography variant="h3">Tecnologias</Typography>
       {techBarList.map(({ techName, completed }, index) => (
         <article key={`techName-${index}`}>
           <ProgressBar completed={completed} techName={techName} />
         </article>
       ))}
+      <Typography variant="h4">Outras tecnologias e habilidades</Typography>
       {techNamesList.map((tech, index) => (
         <p key={`tech-${index}`}>{tech}</p>
       ))}
